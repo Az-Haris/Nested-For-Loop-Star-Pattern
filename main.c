be001735ccd4484bf4cac41fa521f7d90f555ba4
MD Alimuzzaman Haris <73691596+Az-Haris@users.noreply.github.com>
2022-02-02 21:24:04 +0600

    Add files via upload

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+int main()
+{
+    int row, col, n;
+
+    printf("Please Enter Pattern Size Number = ");
+    scanf("%d",&n);
+
+    for(row=1; row<=n; row++)
+    {
+        for(col=1; col<=row; col++)
+        {
+            printf("  *");
+        }
+        printf("\n");
+    }
+
+
+    getch();
+    return 0;
+}
